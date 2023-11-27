973a5a0b7a7b0889916585c5031829c3c2360f4b
Куц Алексей Витальевич <a.kuts@mr.loc>
2023-11-27 13:37:17 +0300

    project done

---
+++ src/components/About.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-10'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id velit eaque assumenda quibusdam eius asperiores! Minima eveniet laudantium beatae voluptas, vitae ratione, at nesciunt blanditiis nostrum illum doloribus magni, nobis labore eum eaque exercitationem. Quis perspiciatis, hic accusamus magni maiores animi! Perspiciatis neque quibusdam aut expedita sequi sunt deserunt cum!
+            </p>
+
+            <br/>
+
+            <p className='text-xl'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam quis ut illum natus! Impedit asperiores minus laudantium magnam deleniti. Aut ea, eum reiciendis aperiam numquam sit accusamus molestias molestiae quasi tempore qui. Eum delectus at dignissimos repellat quidem soluta voluptatem deserunt, illo minima, natus ratione cum sed repellendus ducimus amet?
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
